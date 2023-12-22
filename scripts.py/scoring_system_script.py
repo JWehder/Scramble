2156592c8d2aabe35ea706cb7c63f9deb1ff0421
Jake Wehder <108289690+JWehder@users.noreply.github.com>
2023-12-22 13:22:07 -0500

    testing API pull and ways to clean data from sportsdata API

---
+++ scripts.py/scoring_system_script.py
@@ -0,0 +1,5 @@
+from services.Scoring_System import ScoringSystem
+import os
+
+os.getenv('SPORTS_API_KEY')
+
--- services/scoring_system.py
+++ services/scoring_system.py
@@ -1,19 +1,24 @@
 import Base
 
 class ScoringSystem(Base):
-    def __init__(self):
+    def __init__(self, **kwargs):
         # Initialization code
-        pass
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        
 
     def fetch_data(self, url):
         response = super().fetch_data(url)
         # Additional processing specific to ScoringSystem
-        processed_data = self.process_data(response)
-        return processed_data
+        # processed_data = self.process_data(response)
+        # return processed_data
+        return response
 
     @staticmethod
     def process_data(self, response):
         # Process the response and return processed data
+        # determine how the scores should be calculated
+        # make a call to the present summary method to present the data in some fashion
         pass
 
 
