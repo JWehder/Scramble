3a2c6a3196676e8b34d089cc26bc91db982a4029
trevor dewindt <trevor.dewindt@gmail.com>
2024-01-14 16:52:13 -0700

    connect front end to back end and create new dummy route

--- server/flask_app/config.py
+++ server/flask_app/config.py
@@ -6,7 +6,7 @@ from sqlalchemy import MetaData
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins=["http://127.0.0.1:5173"])
+CORS(app, origins=["http://127.0.0.1:3000"])
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
