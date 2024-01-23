a65843bf82d78310991b828a51902eba04bc57f2
trevor dewindt <trevor.dewindt@gmail.com>
2024-01-22 18:50:59 -0700

    change div background color

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,17 +1,17 @@
 export default function Home() {
     return (
         <div class="mx-56 p-10">
-            <div class="grid mx-28 tb bg-gray-500 h-52 font-bold">
+            <div class="grid mx-28 tb bg-gray-400 h-52 font-bold">
                 <p class="self-center text-center">Banner</p>
             </div>
             <div class="grid grid-cols-3 gap-10 p-10 h-72">
-                <div class="grid rounded bg-gray-500 font-bold">
+                <div class="grid rounded bg-gray-400 font-bold">
                     <p class="self-center text-center">How It Works</p>
                 </div>
-                <div class="grid rounded bg-gray-500 font-bold">
+                <div class="grid rounded bg-gray-400 font-bold">
                     <p class="self-center text-center">Rules of the Game</p>
                 </div>
-                <div class="grid rounded bg-gray-500 font-bold">
+                <div class="grid rounded bg-gray-400 font-bold">
                     <p class="self-center text-center">FAQ</p>
                 </div>
             </div>
