5bec881d09dee18082d7945120f1d5f7baccea49
trevor dewindt <trevor.dewindt@gmail.com>
2024-01-22 18:36:40 -0700

    center text

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,11 +1,19 @@
 export default function Home() {
     return (
         <div class="mx-56 p-10">
-            <div class="mx-28 tb bg-gray-500 h-52 font-bold">Banner</div>
+            <div class="grid mx-28 tb bg-gray-500 h-52 font-bold">
+                <p class="self-center text-center">Banner</p>
+            </div>
             <div class="grid grid-cols-3 gap-10 p-10 h-72">
-                <div class="rounded bg-gray-500 font-bold">How It Works</div>
-                <div class="rounded bg-gray-500 font-bold">Rules of the Game</div>
-                <div class="rounded bg-gray-500 font-bold">FAQ</div>
+                <div class="grid rounded bg-gray-500 font-bold">
+                    <p class="self-center text-center">How It Works</p>
+                </div>
+                <div class="grid rounded bg-gray-500 font-bold">
+                    <p class="self-center text-center">Rules of the Game</p>
+                </div>
+                <div class="grid rounded bg-gray-500 font-bold">
+                    <p class="self-center text-center">FAQ</p>
+                </div>
             </div>
         </div>
     )
