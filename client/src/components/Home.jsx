081055a866f9f37a9acd2a9e15bc671182332695
trevor dewindt <trevor.dewindt@gmail.com>
2024-01-23 18:31:49 -0700

    add login state

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,19 +1,40 @@
-export default function Home() {
+import { useState, useEffect } from 'react'
+import Test from './Test'
+
+export default function Home({ setIsLoggedIn }) {
+    const [test, setTest] = useState([])
+
+    useEffect(() => {
+        fetch('/api/dummy')
+        .then(r => r.json())
+        .then(data => setTest(data))
+        .catch(err => console.log(err))
+    }, [])
+
     return (
-        <div class="mx-56 p-10">
-            <div class="grid mx-28 tb bg-gray-400 h-52 font-bold">
-                <p class="self-center text-center">Banner</p>
+        <div>
+            <h1 className="text-3xl font-bold underline text-center mt-14">
+                Scramble
+            </h1>
+            <p class="text-l font-bold underline text-center mt-14" onClick={() => setIsLoggedIn(false)}>Log Out</p>
+            <div class="mx-56 p-10">
+                <div class="grid mx-28 tb bg-gray-400 h-52 font-bold">
+                    <p class="self-center text-center">Banner</p>
+                </div>
+                <div class="grid grid-cols-3 gap-10 p-10 h-72">
+                    <div class="grid rounded bg-gray-400 font-bold">
+                        <p class="self-center text-center">How It Works</p>
+                    </div>
+                    <div class="grid rounded bg-gray-400 font-bold">
+                        <p class="self-center text-center">Rules of the Game</p>
+                    </div>
+                    <div class="grid rounded bg-gray-400 font-bold">
+                        <p class="self-center text-center">FAQ</p>
+                    </div>
+                </div>
             </div>
-            <div class="grid grid-cols-3 gap-10 p-10 h-72">
-                <div class="grid rounded bg-gray-400 font-bold">
-                    <p class="self-center text-center">How It Works</p>
-                </div>
-                <div class="grid rounded bg-gray-400 font-bold">
-                    <p class="self-center text-center">Rules of the Game</p>
-                </div>
-                <div class="grid rounded bg-gray-400 font-bold">
-                    <p class="self-center text-center">FAQ</p>
-                </div>
+            <div class="text-center">
+                {test.length !== 0 ? test.players.map(e => <Test key={e.player_name} name={e.player_name} />) : null}
             </div>
         </div>
     )
