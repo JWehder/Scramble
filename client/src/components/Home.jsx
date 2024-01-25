7a4744cd609843180160f2cd8179ffca7d286c6b
trevor dewindt <trevor.dewindt@gmail.com>
2024-01-24 20:21:05 -0700

    merge

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import Test from './Test'
+import CreateLeague from './CreateLeague'
 
 export default function Home({ setIsLoggedIn }) {
     const [test, setTest] = useState([])
@@ -17,6 +18,7 @@ export default function Home({ setIsLoggedIn }) {
                 Scramble
             </h1>
             <p class="text-l font-bold underline text-center mt-14" onClick={() => setIsLoggedIn(false)}>Log Out</p>
+            <CreateLeague />
             <div class="mx-56 p-10">
                 <div class="grid mx-28 tb bg-gray-400 h-52 font-bold">
                     <p class="self-center text-center">Banner</p>
