302aedcf87a9e6f9501d6c7ee07cd9279dc7e2c5
trevor dewindt <trevor.dewindt@gmail.com>
2024-02-06 19:55:22 -0700

    replace class with className

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -15,25 +15,25 @@ export default function Home({ setIsLoggedIn }) {
     return (
         <div>
             <h1 className="text-3xl font-bold underline text-center mt-14">Scramble</h1>
-            <p class="text-l font-bold underline text-center mt-14" onClick={() => setIsLoggedIn(false)}>Log Out</p>
+            <p className="text-l font-bold underline text-center mt-14" onClick={() => setIsLoggedIn(false)}>Log Out</p>
             <CreateLeague />
-            <div class="mx-56 p-10">
-                <div class="grid mx-28 tb bg-gray-400 h-52 font-bold">
-                    <p class="self-center text-center">Banner</p>
+            <div className="mx-56 p-10">
+                <div className="grid mx-28 tb bg-gray-400 h-52 font-bold">
+                    <p className="self-center text-center">Banner</p>
                 </div>
-                <div class="grid grid-cols-3 gap-10 p-10 h-72">
-                    <div class="grid rounded bg-gray-400 font-bold">
-                        <p class="self-center text-center">How It Works</p>
+                <div className="grid grid-cols-3 gap-10 p-10 h-72">
+                    <div className="grid rounded bg-gray-400 font-bold">
+                        <p className="self-center text-center">How It Works</p>
                     </div>
-                    <div class="grid rounded bg-gray-400 font-bold">
-                        <p class="self-center text-center">Rules of the Game</p>
+                    <div className="grid rounded bg-gray-400 font-bold">
+                        <p className="self-center text-center">Rules of the Game</p>
                     </div>
-                    <div class="grid rounded bg-gray-400 font-bold">
-                        <p class="self-center text-center">FAQ</p>
+                    <div className="grid rounded bg-gray-400 font-bold">
+                        <p className="self-center text-center">FAQ</p>
                     </div>
                 </div>
             </div>
-            <div class="text-center">
+            <div className="text-center">
                 {test.length !== 0 ? test.players.map(e => <Test key={e.player_name} name={e.player_name} />) : null}
             </div>
         </div>
